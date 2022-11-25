1c0696be70a6182f49f3575d14e7a03b4abccc84
JPenaloza1 <100963782+JPenaloza1@users.noreply.github.com>
2022-11-25 13:06:02 -0600

    Apuntes 24/11/2022

---
+++ src/06-memos/CallbackHook.jsx
@@ -0,0 +1,25 @@
+import { useCallback, useState } from "react"
+import { ShowIncrement } from "./ShowIncrement";
+
+export const CallbackHook = () => {
+
+    const [ counter, setCounter ] = useState( 10 );
+
+    const incrementFather = useCallback( (valor) => {
+        setCounter( ( value ) => value + valor);
+    }, []);
+
+    // const incrementFather = () => {
+    //     setCounter( counter + 1 );
+    // }
+
+    return (
+        <>
+            <h1>useCallback { counter }</h1>
+            <hr />
+
+            <ShowIncrement increment={ incrementFather } />
+        </>
+    )
+}
+
