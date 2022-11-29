9cb18c59080a7ff288c8ee1cf5f5409797ab47f0
JPenaloza1 <100963782+JPenaloza1@users.noreply.github.com>
2022-11-29 09:33:50 -0600

    Apuntes 29/11/2022

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { BrowserRouter } from 'react-router-dom';
+
 // import { CallbackHook } from './06-memos/CallbackHook';
 // import { MemoHook } from './06-memos/MemoHook';
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
@@ -13,14 +15,17 @@ import ReactDOM from 'react-dom/client';
 // import { SimpleForm } from './02-useEffect/SimpleForm';
 // import { HooksApp } from './HooksApp';
 // import { Padre } from './07-tarea-memo/Padre';
-import { TodoApp } from './08-useReducer/TodoApp';
+// import { TodoApp } from './08-useReducer/TodoApp';
+import { MainApp } from './09-useContext/MainApp';
 
 // import './08-useReducer/intro-reducer';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    <TodoApp />
-  // </React.StrictMode>
+  <BrowserRouter>
+    {/* <React.StrictMode> */}
+      <MainApp />
+    {/* </React.StrictMode> */}
+  </BrowserRouter>
 )
